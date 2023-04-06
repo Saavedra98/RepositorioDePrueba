2c6fae60d55d30d5d1192a095e9bbe48027cbcb5
Saavedra98 <saavedraerazoc@gmail.com>
2023-04-06 10:05:18 -0500

    cambios de js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const $tiempo = document.querySelector('.tiempo');
 
 function digitalClock(){
-    let relojito = new Date(),
-    dia = relojito.getDate(),
-    mes = relojito.getMonth(),
-    anio = relojito.getFullYear(),
-    diaSemana = relojito.getDay();
+    let rejoj = new Date(),
+    dia = rejoj.getDate(),
+    mes = rejoj.getMonth(),
+    anio = rejoj.getFullYear(),
+    diaSemana = rejoj.getDay();
 
 
-    let timeString = relojito.toLocaleTimeString();
+    let timeString = rejoj.toLocaleTimeString();
     $tiempo.innerHTML = timeString;
 
 }
